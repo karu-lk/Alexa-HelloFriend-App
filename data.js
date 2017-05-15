0cd2742fc836a1f4ff24338fdc23d2960e778045
Karu <lilupa@live.com>
2017-05-15 15:36:49 +1200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,16 @@
+module.exports.deepthi = [
+    "Hi Deepthi how are you? Hope you are enjoying this sunny weather",
+    "Hello Deepthi how's it going? Hope you are enjoying the summer"
+];
+
+module.exports.chathura = [
+    "Hi Chathura how are you doing? Hope you are enjoying this summer"
+];
+
+module.exports.chetha = [
+    "Hi Chetha how are you doing? Hope you are enjoying your new house"
+];
+
+module.exports.akithma = [
+    "Hi Akithma how are you doing girl? How is your day with your friends?"
+];
--- lambda-function.js
+++ lambda-function.js
@@ -1,62 +1,67 @@
-exports.handler = (event, context) => {
-
-  try {
-
-    if (event.session.new) {
-      // New Session
-      console.log("NEW SESSION")
-    }
-
-    switch (event.request.type) {
-
-      case "LaunchRequest":
-        // Launch Request
-        console.log(`LAUNCH REQUEST`)
-        context.succeed(
-          generateResponse(
-            buildSpeechletResponse("Hi Lilupa how are you. Hope you are doing good.", true),
-            {}
-          )
-        )
-        break;
-
-      case "IntentRequest":
-        // Intent Request
-        console.log(`INTENT REQUEST`)
-			break;
-			
-			case "SessionEndedRequest":
-        // Session Ended Request
-        console.log(`SESSION ENDED REQUEST`)
-        break;
-
-      default:
-        context.fail(`INVALID REQUEST TYPE: ${event.request.type}`)
-		}
-	}
-	catch(error) { context.fail(`Exception: ${error}`) }
-};
-
-//helpers
-
-buildSpeechletResponse = (outputText, shouldEndSession) => {
-
-  return {
-    outputSpeech: {
-      type: "PlainText",
-      text: outputText
+var Alexa = require('alexa-sdk');
+var Data = require("./data");
+ 
+const skillName = "Buddy";
+ 
+var handlers = {
+ 
+    "WelcomeIntent": function () {
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
+        var speechOutput = "";
+        if(this.event.request.intent.slots.FriendName.value && this.event.request.intent.slots.FriendName.value.toLowerCase() == "deepthi") {
+            speechOutput = Data.deepthi[getRandomInt(0, 2)];
+        } else if(this.event.request.intent.slots.FriendName.value && this.event.request.intent.slots.FriendName.value.toLowerCase() == "chathura") {
+            speechOutput = Data.chathura[getRandomInt(0, 1)];
+        } else if(this.event.request.intent.slots.FriendName.value && this.event.request.intent.slots.FriendName.value.toLowerCase() == "chetha") {
+            speechOutput = Data.chetha[getRandomInt(0, 1)];
+        } else if(this.event.request.intent.slots.FriendName.value && this.event.request.intent.slots.FriendName.value.toLowerCase() == "akithma") {
+            speechOutput = Data.akithma[getRandomInt(0, 1)];
+        } else {
+            speechOutput = "I don't have anything interesting to share regarding what you've asked."
+        }
+        this.emit(':tellWithCard', speechOutput, skillName, speechOutput);
     },
-    shouldEndSession: shouldEndSession
-  }
-
-}
-
-generateResponse = (speechletResponse, sessionAttributes) => {
-
-  return {
-    version: "1.0",
-    sessionAttributes: sessionAttributes,
-    response: speechletResponse
-  }
-
-}
+ 
+    "AboutIntent": function () {
+        var speechOutput = "The " + skillName + " Developer, Lilupa, is from Wellington, New Zealand";
+        this.emit(':tellWithCard', speechOutput, skillName, speechOutput);
+    },
+ 
+    "AMAZON.HelpIntent": function () {
+        var speechOutput = "";
+        speechOutput += "Here are some things you can say: ";
+        speechOutput += "Tell me something interesting about Java. ";
+        speechOutput += "Tell me about the skill developer. ";
+        speechOutput += "You can also say stop if you're done. ";
+        speechOutput += "So how can I help?";
+        this.emit(':ask', speechOutput, speechOutput);
+    },
+ 
+    "AMAZON.StopIntent": function () {
+        var speechOutput = "Goodbye and best wishes from " + skillName;
+        this.emit(':tell', speechOutput);
+    },
+ 
+    "AMAZON.CancelIntent": function () {
+        var speechOutput = "Goodbye and best wishes from " + skillName;
+        this.emit(':tell', speechOutput);
+    },
+ 
+    "LaunchRequest": function () {
+        var speechText = "";
+        speechText += "Welcome to " + skillName + ".  ";
+        speechText += "You can ask " + skillName + " to welcome your friends such as welcome John.";
+        var repromptText = "For instructions on what you can say, please say help me.";
+        this.emit(':ask', speechText, repromptText);
+    }
+ 
+};
+ 
+exports.handler = function (event, context) {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = "amzn1.ask.skill.cc1e223d-3fff-4656-838d-d40ec9143a54";
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
